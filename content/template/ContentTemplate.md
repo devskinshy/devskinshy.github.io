0c9a8c45ddb6e88c1dd6f677163c4283835ede4c
devskinshy <yyshgk12@gmail.com>
2023-12-31 19:09:27 +0900

    vault backup: 2023-12-31 19:09:27

---
+++ content/template/ContentTemplate.md
@@ -0,0 +1,13 @@
+---
+title: <% tp.file.cursor(1) %>
+date: <% tp.file.creation_date() %>
+tags: []
+---
+
+<% tp.web.random_picture("768x432") %>
+
+
+
+---
+
+<% tp.web.daily_quote() %>
--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 const { withContentlayer } = require("next-contentlayer");
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  ...((isGithubActions || isProduction) && {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  })
 };
 
 module.exports = withContentlayer(nextConfig);
